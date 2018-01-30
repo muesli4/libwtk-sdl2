ae6803dd96bc213e66d2cec830e5030b4a9b7a01
Moritz Bruder <muesli4@gmail.com>
2018-01-30 07:50:16 +0100

    Add override specifier

--- button.hpp
+++ button.hpp
@@ -9,9 +9,9 @@ struct button : widget
 {
     button(std::string text, std::function<void()> callback);
 
-    void on_draw(draw_context & dc) const;
-    void on_mouse_event(mouse_event const & e);
-    void on_key_event(key_event const & e);
+    void on_draw(draw_context & dc) const override;
+    void on_mouse_event(mouse_event const & e) override;
+    void on_key_event(key_event const & e) override;
 
     private:
 
